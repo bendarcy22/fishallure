a6faa7bcc1b351fb5526d285356c591d260c628d
Ben Darcy <BenDarcy@Ben-Darcys-MacBook-Pro.local>
2016-07-19 12:59:40 -0400

    added some testing for log in and out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,15 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-  
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+def log_in_user
+  visit new_user_registration_path
+  fill_in 'Email', with: 'catchallthefish@gmail.com'
+  fill_in 'Username', with: 'catchallthefish'
+  fill_in 'Password', with: 'password'
+  fill_in 'Password confirmation', with: 'password'
+  click_on 'Sign up'
 end
