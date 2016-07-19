269bbd5d14e10a1165eef924622c7af74c30dd1b
Ben Darcy <BenDarcy@Ben-Darcys-MacBook-Pro.local>
2016-07-19 11:38:18 -0400

    basic lure page

--- app/controllers/lures_controller.rb
+++ app/controllers/lures_controller.rb
@@ -2,11 +2,15 @@ class LuresController < ApplicationController
   def index
     @lures = Lure.all
   end
-  
+
   def hot
     @catches = Catch.all
     @lures = Hash.new(0)
     @catches.each { |c| @lures[c.lure] += 1 }
     @hottest_lures = @lures.sort_by{ |_k, v| v }.reverse
+  end
+
+  def show
+    @lure = Lure.find(params[:id])
   end
 end
