49db7aab44fc0aaf7226438052bf6290e668bc78
Ben Darcy <BenDarcy@Ben-Darcys-MacBook-Pro.local>
2016-07-19 09:25:51 -0400

    added hottest lures page

--- app/controllers/lures_controller.rb
+++ app/controllers/lures_controller.rb
@@ -2,10 +2,11 @@ class LuresController < ApplicationController
   def index
     @lures = Lure.all
   end
+  
   def hot
     @catches = Catch.all
     @lures = Hash.new(0)
     @catches.each { |c| @lures[c.lure] += 1 }
-    @hottest_lures = @lures.sort_by{|k, v| v}.reverse
+    @hottest_lures = @lures.sort_by{ |_k, v| v }.reverse
   end
 end
