8d047fc49fb481e34edd736c9a3da59421cf244b
Ben Darcy <BenDarcy@Ben-Darcys-MacBook-Pro.local>
2016-07-20 17:32:50 -0400

    added form for creating fish

--- app/controllers/fish_types_controller.rb
+++ app/controllers/fish_types_controller.rb
@@ -1,9 +1,11 @@
 class FishTypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :hot, :show]
+
   def hot
     @catches = Catch.all
     @fishes = Hash.new(0)
     @catches.each { |c| @fishes[c.fish_type] += 1 }
-    @hottest_fishes = @fishes.sort_by{ |_k, v| v }.reverse
+    @hottest_fishes = @fishes.sort_by { |_k, v| v }.reverse
   end
 
   def show
@@ -12,5 +14,36 @@ class FishTypesController < ApplicationController
     sorted_lures = Hash.new(0)
     lures.each { |lure| sorted_lures[lure] += 1 }
     @lures_for_fish = sorted_lures.sort_by { |_k, v| v }.reverse
+  end
+
+  def new
+    @fish = FishType.new
+  end
+
+  def create
+    @fish = FishType.new(fish_params)
+    if FishType.find_by(name: params[:fish_type][:name]) == nil
+      @fish.user = current_user
+      if @fish.save
+        flash[:notice] = "The Fish has been added!"
+        redirect_to fish_type_path(@fish)
+      else
+        flash[:errors] = @fish.errors.full_messages.join(", ")
+        render :new
+      end
+    else
+      flash[:errors] = "That fish is already in the database"
+      render :new
+    end
+  end
+
+  private
+
+  def fish_params
+    params.require(:fish_type).permit(
+      :name,
+      :description,
+      :fish_photo
+    )
   end
 end
