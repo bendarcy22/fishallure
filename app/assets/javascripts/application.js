5c72770c7320eb4801e3d11dea907c4b2e957434
Ben Darcy <BenDarcy@Ben-Darcys-MacBook-Pro.local>
2016-07-21 20:44:47 -0400

    google maps trial phase

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,33 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+var marker;
+var map;
+var clicked = false;
+function initMap() {
+  var myLatLng = {lat: 42.3601, lng: -71.0589};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 12
+  });
+  google.maps.event.addListener(map, 'click', function(event) {
+    if(clicked === false){
+      addMarker(event.latLng, map);
+      clicked = true;
+    }
+  });
+}
+function addMarker(location, map) {
+  marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+  });
+}
+function getCatchLocation() {
+  var lat = marker.getPosition().lat();
+  var lng = marker.getPosition().lng();
+}
+$('#add-catch').on('click', getCatchLocation);
+google.maps.event.addDomListener(window, 'load', initialize);
